64b88629d1605c72cb91a3d3de7b1c89a0e95792
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-08-18 23:34:00 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 import { Inter } from "next/font/google";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Lets build Netflix
-      <main></main>
+      <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
     </div>
   );
 };
