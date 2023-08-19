acb05819063bfb52229466717cb4510058dabe2e
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-08-19 09:57:21 +0530

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@ import { Inter } from "next/font/google";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>
-        {/* Banner */}
+        <Banner />
         <section>
           {/* Row */}
           {/* Row */}
